7f2468b9d58376c4f42567d2581af65181f7d48b
Arapoglu, Simon <simon.arapoglu@student.fhws.de>
2021-08-21 17:38:16 +0200

    Implemented Basic Design

---
+++ World Launcher/Form1.Designer.cs
@@ -0,0 +1,159 @@
+﻿
+namespace World_Launcher
+{
+    partial class WorldLauncher
+    {
+        /// <summary>
+        /// Erforderliche Designervariable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Verwendete Ressourcen bereinigen.
+        /// </summary>
+        /// <param name="disposing">True, wenn verwaltete Ressourcen gelöscht werden sollen; andernfalls False.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Vom Windows Form-Designer generierter Code
+
+        /// <summary>
+        /// Erforderliche Methode für die Designerunterstützung.
+        /// Der Inhalt der Methode darf nicht mit dem Code-Editor geändert werden.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lv_game = new System.Windows.Forms.ListView();
+            this.ch_file = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.openFileDialog2 = new System.Windows.Forms.OpenFileDialog();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.SuspendLayout();
+            // 
+            // lv_game
+            // 
+            this.lv_game.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.ch_file});
+            this.lv_game.Cursor = System.Windows.Forms.Cursors.Default;
+            this.lv_game.HideSelection = false;
+            this.lv_game.Location = new System.Drawing.Point(12, 95);
+            this.lv_game.MultiSelect = false;
+            this.lv_game.Name = "lv_game";
+            this.lv_game.Size = new System.Drawing.Size(760, 354);
+            this.lv_game.TabIndex = 0;
+            this.lv_game.UseCompatibleStateImageBehavior = false;
+            this.lv_game.View = System.Windows.Forms.View.List;
+            this.lv_game.SelectedIndexChanged += new System.EventHandler(this.lv_game_SelectedIndexChanged);
+            // 
+            // ch_file
+            // 
+            this.ch_file.Text = "File";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 24);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(116, 41);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Choose SMW Rom Location";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(134, 24);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(116, 41);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Choose Patch Folder";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // openFileDialog2
+            // 
+            this.openFileDialog2.FileName = "openFileDialog2";
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(656, 24);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(116, 41);
+            this.button4.TabIndex = 4;
+            this.button4.Text = "Patch!";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(256, 24);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(116, 41);
+            this.button3.TabIndex = 6;
+            this.button3.Text = "Choose Emulator";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Checked = true;
+            this.checkBox1.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.checkBox1.Location = new System.Drawing.Point(378, 37);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(168, 17);
+            this.checkBox1.TabIndex = 7;
+            this.checkBox1.Text = "Delete Patches after Patching";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
+            // WorldLauncher
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(784, 461);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.lv_game);
+            this.MaximumSize = new System.Drawing.Size(800, 500);
+            this.MinimumSize = new System.Drawing.Size(800, 500);
+            this.Name = "WorldLauncher";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load_1);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListView lv_game;
+        private System.Windows.Forms.ColumnHeader ch_file;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog2;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.CheckBox checkBox1;
+    }
+}
+
---
+++ World Launcher/Form1.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Diagnostics;
+
+namespace World_Launcher
+{
+    public partial class WorldLauncher : Form
+    {
+        string smwRomPath = null;
+        string emulatorPath = null;
+        string patchFolderPath = null;
+
+        public WorldLauncher()
+        {
+            InitializeComponent();
+            this.Text = "World Launcher";
+        }
+
+        private void Form1_Resize(object sender, System.EventArgs e)
+        {
+            Control control = (Control)sender;
+
+            // Ensure the Form remains square (Height = Width).
+            if (control.Size.Height != control.Size.Width)
+            {
+                control.Size = new Size(control.Size.Width, control.Size.Width);
+            }
+
+            //TODO: Scale everything based on window size
+
+            /*float scaleX = ((float)formNewWidth / formBaseWidth);
+            float scaleY = ((float)formNewHeight / formBaseHeight);
+            this.Scale(new SizeF(scaleX, scaleY));*/
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void Form1_Load_1(object sender, EventArgs e)
+        {
+            //TODO: create internal folder to store patched games
+            DisplayFolder("C:\\Users\\arapo_000\\Desktop\\Neuer Ordner", lv_game);
+
+        }
+
+        public void DisplayFolder(string folderPath, ListView lv)
+        {
+            string[] files = System.IO.Directory.GetFiles(folderPath);
+
+            for (int x = 0; x < files.Length; x++)
+            {
+                lv.Items.Add(files[x].Substring(folderPath.Length+1));
+            }
+        }
+
+        private void lv_game_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            openFileDialog1.ShowDialog();
+            smwRomPath = openFileDialog1.FileName;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            folderBrowserDialog1.ShowDialog();
+            patchFolderPath = folderBrowserDialog1.SelectedPath;
+        }
+
+        
+        private void button3_Click(object sender, EventArgs e)
+        {
+            openFileDialog1.ShowDialog();
+            emulatorPath = openFileDialog2.FileName;
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            if (smwRomPath == null)
+            {
+                MessageBox.Show("Location of the SMW Rom has not been chosen yet.", "Error",
+                MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            else if(patchFolderPath == null)
+            {
+                MessageBox.Show("Folder containing patches has not been chosen yet.", "Error",
+                MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+                else if(emulatorPath == null)
+            {
+                MessageBox.Show("Location of the Emulator has not been chosen yet.", "Error",
+                MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+
+            else
+            {
+                
+                //TODO: Patch files
+            }
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+           
+        }
+
+
+
+    }
+}
